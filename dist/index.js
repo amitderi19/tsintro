bebbcb656604d01aaf1ebdf697ebc088e6fe5711
amitderi19 <amitderi780@gmail.com>
2023-01-12 09:25:34 +0200

    function get average

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,17 @@
 "use strict";
-const age = 10;
-console.log(age);
+function getAverage(array) {
+    let avg = 0, countNumbers = 0;
+    ;
+    array.forEach(function (value) {
+        avg = avg + value;
+        countNumbers++;
+    });
+    alert(avg / countNumbers);
+}
+let number = Number(prompt("enter number, if you want to stop, enter -1"));
+let array = new Array;
+while (number != -1) {
+    array.push(number);
+    number = Number(prompt("enter number, if you want to stop, enter -1"));
+}
+getAverage(array);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const age:number=10;
-console.log(age);
+
+function getAverage(array : number[]): void{
+    let avg : number =0, countNumbers=0;;
+    array.forEach(function(value){
+        avg=avg+value;
+        countNumbers++;
+    })
+    alert(avg/countNumbers);
+}
+let number : number = Number(prompt("enter number, if you want to stop, enter -1"));
+let array : number[] = new Array;
+while(number!=-1){
+    array.push(number);
+    number=Number(prompt("enter number, if you want to stop, enter -1"));
+}
+getAverage(array);
