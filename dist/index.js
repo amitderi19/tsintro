1edda5eab52aabb8ec9986d51c4c7696522180a5
amitderi19 <amitderi780@gmail.com>
2023-01-12 10:28:48 +0200

    function sort list

--- dist/index.js
+++ dist/index.js
@@ -31,3 +31,32 @@ while (number != 0) {
     number = Number(prompt("enter number, if you want to stop, enter 0"));
 }
 getAmountOfPositive(array);
+function findLowest(array) {
+    let lowestNumber = array[0], lowestNumberIndex = 0, i = 0;
+    array.forEach(function (value) {
+        if (value < lowestNumber) {
+            lowestNumber = value;
+            lowestNumberIndex = i;
+        }
+        i++;
+    });
+    return lowestNumberIndex;
+}
+function sortList(array) {
+    let sortedArray = new Array;
+    while (array.length != 0) {
+        let lowestNumberIndex = findLowest(array);
+        sortedArray.push(array[lowestNumberIndex]);
+        delete array[lowestNumberIndex];
+    }
+    array.forEach(function (value) {
+        alert(value);
+    });
+}
+number = Number(prompt("enter number, if you want to stop, enter 0"));
+array = new Array;
+while (number != 0) {
+    array.push(number);
+    number = Number(prompt("enter number, if you want to stop, enter 0"));
+}
+sortList(array);
--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,38 @@ while(number!=0){
     number=Number(prompt("enter number, if you want to stop, enter 0"));
 }
 getAmountOfPositive(array);
+
+function findLowest(array : number[]) : number{
+    let lowestNumber:number=array[0], lowestNumberIndex:number=0, i=0;
+    array.forEach(function(value){
+        if(value<lowestNumber){
+            lowestNumber=value;
+            lowestNumberIndex=i;
+        }
+        i++
+    })
+    return lowestNumberIndex;
+}
+
+function sortList(array : number[]): void{
+    let sortedArray : number[] =new Array;
+    while(array.length!=0){
+        let lowestNumberIndex=findLowest(array);
+        sortedArray.push(array[lowestNumberIndex]);
+        delete array[lowestNumberIndex];
+    }
+    array.forEach(function(value){
+        alert(value)
+    })
+}
+
+
+number = Number(prompt("enter number, if you want to stop, enter 0"));
+array = new Array;
+while(number!=0){
+    array.push(number);
+    number=Number(prompt("enter number, if you want to stop, enter 0"));
+}
+sortList(array);
+
+    
